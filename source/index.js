f7c5b1bf4ae7837474921de23ff3ce10ee9e1aed
Jing-Yuan <a13114337@gmail.com>
2014-08-17 10:20:13 +0800

    update

--- source/index.js
+++ source/index.js
@@ -1,13 +1,13 @@
 $(function() {
     $('#install').click( function() { 
-      $.ajax({ url: "source/install.html",  type:"GET",
+      $.ajax({ url: "source/install.html",  type:"GET", cache: false,
         success: function(msg){
           document.getElementById("content").innerHTML=msg;
         }});
       });
     
     $('#startghPages').click( function() {
-      $.ajax({ url: "source/startghPages.html",  type:"GET",
+      $.ajax({ url: "source/startghPages.html",  type:"GET", cache: false,
         success: function(msg){
           document.getElementById("content").innerHTML=msg;
         }});
